2236f5975c40fc4a0bf9724f52c0b611165f63be
Billy Fletcher <Billyfletcher99@gmail.com>
2022-10-27 23:09:09 -0400

    2nd push, added Main.jsx and spiffed up Navbar lil

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+<div name="Main" className="bg-white w-full h-screen">
+  <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8"></div>
+</div>
+  )
+}
+
+export default Main
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,11 @@
 import React from 'react'
-import { Link } from 'react-scroll';
+
 
 const NavBar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-neutral-900 text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-green-300 text-gray-300 rounded-b-3xl'>
      {/* navbar */}
-     <ul className='hidden md:flex'>
-         <li className='tracking-widest px-4 cursor-pointer text-slate-400 hover:text-zinc-300'>
-            <Link to='home' smooth={true} duration={500}> Home </Link>
-         </li>
-         <li className='tracking-widest px-4 cursor-pointer text-slate-400 hover:text-zinc-300'>
-            <Link to='about' smooth={true} duration={500}> About </Link>
-         </li>
-         <li className='tracking-widest px-4 cursor-pointer text-slate-400 hover:text-zinc-300'>
-            <Link to='skills' smooth={true} duration={500}> Skills </Link>
-         </li>
-         <li className='tracking-widest px-4 cursor-pointer text-slate-400 hover:text-zinc-300'>
-            <Link to='work' smooth={true} duration={500}> Work </Link>
-         </li>
-         <li className='tracking-widest px-4 cursor-pointer text-slate-400 hover:text-zinc-300'>
-            <Link to='contact' smooth={true} duration={500}> Contact </Link>
-         </li>
-      </ul>
+     <h1 className='md:p-2 lg:px-4 font-semibold block text-green-50 text-3xl py-1 absolute right-0'>A New leaf</h1>
     </div>
   )
 }
