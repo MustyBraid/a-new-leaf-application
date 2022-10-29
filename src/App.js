c00cb8e93fe5540e9573bdee8378d61be7627e88
Musty Braid <chartiercasey@gmail.com>
2022-10-29 13:12:15 -0400

    built a 404 and connected it to the router. Styling is exceptionally ugly

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 import Editor from "./components/Editor";
 import Signup from "./components/Signup";
+import PageNotFound from "./components/404";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/editPlant" element={<Editor />} />
         <Route path="/signIn" element={<Signup />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
