12b4466028c7f7d3ec275bd7a92c57363e69e7ff
calihuddleston <calihuddleston@gmail.com>
2022-10-29 13:20:12 -0400

    build: begin writing database seeds and queries

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,11 +1,24 @@
 const db = require("../config/connection");
-const { User } = require("../models");
+const { User, Plant } = require("../models");
 const userSeeds = require("./userSeeds.json");
+const plantSeeds = require("./plantSeeds.json");
 
 db.once("open", async () => {
   try {
     await User.deleteMany({});
     await User.create(userSeeds);
+
+    console.log("all done!");
+    process.exit(0);
+  } catch (err) {
+    throw err;
+  }
+});
+
+db.once("open", async () => {
+  try {
+    await Plant.deleteMany({});
+    await Plant.create(userSeeds);
 
     console.log("all done!");
     process.exit(0);
