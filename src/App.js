81778ceb1f86183d03176c5bc25404dbe9075c18
Billy Fletcher <Billyfletcher99@gmail.com>
2022-10-29 14:04:25 -0400

    Hopefully fixed some errors with pages

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
+import Editor from "./components/Editor";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Main />
-      <Footer />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/editplant" element={<Editor />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<PageNotFound />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
