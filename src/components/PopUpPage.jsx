809d96712ad748c2593c824daac95f70d2cf37e5
Billy Fletcher <Billyfletcher99@gmail.com>
2022-11-01 19:12:31 -0400

    latest updates on popup

--- src/components/PopUpPage.jsx
+++ src/components/PopUpPage.jsx
@@ -18,6 +18,9 @@ const popUpPage = () => {
    <div class="bg-gray-400 w-64 h-3 rounded-lg mt-2 overflow-hidden">
     <div class="bg-green-400 w-3/4 h-full rounded-lg shadow-md"></div>
    </div>
+   <div className="box-border p-5 pb-4 border-2 rounded-lg bg-white m4 mt-4">
+    <h3 className='text-sm text-left pb-2'>Watering</h3>
+   </div>
   </div>
  </div>
 </div>
