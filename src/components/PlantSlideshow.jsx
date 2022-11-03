88ccad7370da46b9770109c42e868551ec9c93a9
B McCool <mccoolcontact@gmail.com>
2022-11-03 20:37:36 -0300

    ui edit

--- src/components/PlantSlideshow.jsx
+++ src/components/PlantSlideshow.jsx
@@ -63,8 +63,7 @@ const ImageSlider = () => {
             <label for="plantNSame">
               <p className='text-zinc-400 text-lg'>Hercules</p>
               <input className="placeholder:italic placeholder:text-zinc-400 text-lg block bg-white w-30 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 invalid:border-red-500 focus:ring-1 " placeholder="Hercules" type="text" name="search"/>
-            </label>
-    
+            </label>    
 
             {/* selected species */}
             <div className="border-t border-solid mt-6 md:mt-12 pt-6"></div>
