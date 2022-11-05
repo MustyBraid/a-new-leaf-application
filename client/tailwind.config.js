dc86456e862351c60260d47a9e98a4bfc8b9fe81
Musty Braid <chartiercasey@gmail.com>
2022-11-05 11:40:49 -0400

    Tailwing please read from the html I beg youuu

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}", "./public/*.{js,jsx,html}"],
   theme: {
     borderRadius: {
       none: "0",
